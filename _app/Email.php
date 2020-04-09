a6921dfb3d4d6caa3d210b2530919c7bf4211325
BarrosEdimar <edimarbarros90@gmail.com>
2020-04-09 14:59:05 -0300

    Library OK

--- _app/Email.php
+++ _app/Email.php
@@ -8,22 +8,22 @@ use PHPMailer\PHPMailer\Exception;
 class Email {
     private $mail = \stdClass::class;
 
-    public function __construct() {
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName) {
         $this->mail = new PHPMailer(true);
 
         //Server settings
-        $this->mail->SMTPDebug = 2;
+        $this->mail->SMTPDebug = $smtpDebug;
         $this->mail->isSMTP();
-        $this->mail->Host       = 'smtp.hostinger.com.br';
+        $this->mail->Host       = $host;
         $this->mail->SMTPAuth   = true;
-        $this->mail->Username   = 'edimarbarros@hode.com.br';
-        $this->mail->Password   = '';
-        $this->mail->SMTPSecure = 'tsl';
-        $this->mail->Port       = 587;
-        $this->mail->CharSet = 'utf-8';
+        $this->mail->Username   = $user;
+        $this->mail->Password   = $pass;
+        $this->mail->SMTPSecure = $smtpSecure;
+        $this->mail->Port       = $port;
+        $this->mail->CharSet    = 'utf-8';
         $this->mail->setLanguage('br');
         $this->mail->isHTML(true);
-        $this->mail->setFrom('edimarbarros@hode.com.br', 'Edimar Barros');
+        $this->mail->setFrom($setFromEmail, $setFromName);
     }
 
     public function sendMail($subject, $body, $replyEmail, $replyName, $addressEmail, $addressName) {
--- test/index.php
+++ test/index.php
@@ -1,9 +1,19 @@
 <?php
-require __DIR__ . '/lib_ext/autoload.php';
+require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email;
+$novoEmail = new Email(
+    2,
+    "smtp.hostinger.com.br",
+    "edimarbarros@hode.com.br",
+    "T3ch3xpr3ss",
+    "tsl",
+    587,
+    "edimarbarros@hode.com.br",
+    "Edimar Barros"
+);
+
 $novoEmail->sendMail(
     "Asusto test",
     "<p><strong>Email</strong> test</p>",
